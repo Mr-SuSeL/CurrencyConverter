2e5f840f3819b0a374e4c1d2e23e5940065a6ea8
Łukasz Susłowicz <susel.invest@gmail.com>
2022-12-30 22:39:53 +0100

    Refctoring to function structure

--- js/script.js
+++ js/script.js
@@ -1,55 +1,59 @@
-let numberToConverse = document.querySelector(".js-numberToConverse"); //wartość wpisana przez użytkownika
-
-let currencyElement = document.querySelector ("select.form__field"); //wybor waluty
-
-let counterElement = document.querySelector(".js-counter"); //oblicz
-
-let formElement = document.querySelector(".js-form"); //cały formularz
-
-// let numberToConverseVal = numberToConverse.value;
-// let currencyCourseVal = currencyCourse.value;
+{
 
 
-let button = document.querySelector(".js-submit");
+    const counter = (amount, currency) => {
+
+        const rateUSD = 4.4018;
+        const rateEUR = 4.6899;
+        const rateGBP = 5.2957;
+        const rateCHF = 4.7679;
+
+        switch (currency) {
+            case "USD":
+                return amount / rateUSD;
+
+            case "EUR":
+                return amount / rateEUR;
+
+            case "GBP":
+                return amount / rateGBP;
+
+            case "CHF":
+                return amount / rateCHF;
+        }
+    };
 
 
+    const makeResult = (amount, result, currency) => {
+        const counterElement = document.querySelector(".js-counter"); //oblicz
+        counterElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
+    }
 
-const rateUSD = 4.4018;
-const rateEUR = 4.6899;
-const rateGBP = 5.2957;
-const rateCHF = 4.7679;
-
-formElement.addEventListener("submit", (event) => {
+   const onChangeSubmit = (event) => {
     event.preventDefault();
 
-    let amount = +numberToConverse.value;
-let currency = currencyElement.value;
-let result;
+    const numberToConverse = document.querySelector(".js-numberToConverse"); //wartość wpisana przez użytkownika
+    const currencyElement = document.querySelector("select.form__field"); //wybor waluty
+    
 
-    switch (currency) {
-        case "USD":
-            result = amount / rateUSD;
-            break;
-        
-        case "EUR": 
-            result = amount / rateEUR;
-            break;
+    const amount = +numberToConverse.value;
+    const currency = currencyElement.value;
+    const result = counter (amount, currency);
+    makeResult(amount, result, currency);
 
-        case "GBP":
-            result = amount / rateGBP;
-            break;
+};
 
-        case "CHF": 
-            result = amount / rateCHF;
-            break;
-    }
-counterElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
-//result;
-//`${result} ${currency}`;
-//`${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
+//amount, currency, result
+    const init = () => {
 
-});
+        const formElement = document.querySelector(".js-form"); //cały formularz
 
+        formElement.addEventListener("submit", onChangeSubmit);
 
+        };
+
+        init();
+
+}
 
 
